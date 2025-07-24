32057d9ddcfa5bb5ae923a4b5733a6ddbbd29e3d
Bhushan R Naik <naikb4800@gmail.com>
2025-07-24 23:14:20 +0530

    Added Bubble Sort(Descending Order) with logs

---
+++ sorting/bubbleSortExtras.js
@@ -0,0 +1,14 @@
+function bubbleSortDescending(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+let arr = [10, 20, 5, 8, 70];
+console.log(bubbleSortDescending(arr));
