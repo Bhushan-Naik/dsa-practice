0540627eb9b95bccd9f04f0c8b307543faccb561
Bhushan R Naik <naikb4800@gmail.com>
2025-07-31 22:41:58 +0530

    code to find if array ia sorted or not

---
+++ arrays/findsortedarray.js
@@ -0,0 +1,13 @@
+
+    function isSorted(nums) {
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return false; // If any element is greater than the next, it's not sorted
+            }
+        }
+        return true; // All elements are in non-decreasing order
+    }
+    console.log(isSorted([1,2,3,4]));
+    console.log(isSorted([2,5,6,7,8,9]));
+    console.log(isSorted([2,5,7,8,9,3,2]));
+    console.log(isSorted([12,14,17,22,27,35,11]));
