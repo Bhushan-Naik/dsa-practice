35b0f92c8b5f1b31d090a9afd8d443dc65dc5c69
Bhushan R Naik <naikb4800@gmail.com>
2025-08-04 22:33:13 +0530

    updated finding a one missiing number in a sorted array

--- arrays/findMissing.js
+++ arrays/findMissing.js
@@ -8,17 +8,17 @@ function findMissingNumber(arr) {
 console.log(findMissingNumber([1, 2, 3, 5, 6])); // Output: 4
 
 
-// function findMissingNumber(arr) {
-//   let expected = 0, actual = 0;
+function findMissingNumber2(arr) {
+  let expected = 0, actual = 0;
 
-//   for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
-//     expected += i;
-//   }
+  for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+    expected += i;
+  }
 
-//   for (let num of arr) {
-//     actual += num;
-//   }
+  for (let num of arr) {
+    actual += num;
+  }
 
-//   return expected - actual;
-// }
-//console.log(findMissingNumber([1, 2, 3, 5, 6])); // Output: 4
+  return expected - actual;
+}
+console.log(findMissingNumber2([1, 2, 3, 5, 6])); // Output: 4
