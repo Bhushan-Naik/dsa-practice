e81d21497565d696ff480f106c8103e9b8f74c4d
Bhushan R Naik <naikb4800@gmail.com>
2025-08-09 23:41:44 +0530

    sort zero ones twos code one is mine technically wrong but right output and actual code with o(N)   requirement

---
+++ arrays/sortZeroOneTwos.js
@@ -0,0 +1,44 @@
+
+// BRN Code
+function sortZeroOneTwos(arr){
+    let temp=[]
+    for(let i = 0; i< arr.length-1; i++){
+        for(let j = 0; j< arr.length; j++){
+            if( i === arr[j])
+            {
+               temp.push(arr[j]); 
+            }
+        }
+    }
+    return temp;
+}
+console.log(sortZeroOneTwos([0,1,2,1,0,2,0,0,0,1,1,1,2]));
+
+
+
+// Actual solutionn
+
+function sortZeroOneTwos(arr) {
+  let low = 0, mid = 0, high = arr.length - 1;
+
+  while (mid <= high) {
+    if (arr[mid] === 0) {
+      // Swap arr[low] and arr[mid]
+      [arr[low], arr[mid]] = [arr[mid], arr[low]];
+      low++;
+      mid++;
+    }
+    else if (arr[mid] === 1) {
+      mid++;
+    }
+    else { // arr[mid] === 2
+      // Swap arr[mid] and arr[high]
+      [arr[mid], arr[high]] = [arr[high], arr[mid]];
+      high--;
+    }
+  }
+  return arr;
+}
+
+// Example:
+console.log(sortZeroOneTwos([0,1,2,1,0,2,0,0,0,1,1,1,2]));
